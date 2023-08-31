aec44f5ba64002a97b8df6512b19e51adccca9a5
andreaslopez5257 <116468133+andreaslopez5257@users.noreply.github.com>
2023-09-01 00:46:23 +0100

    _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -23,11 +23,22 @@ class MyDocument extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
-		      <script data-ad-client="ca-pub-5021867083203973" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          <script data-ad-client="ca-pub-5021867083203973" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js%22%3E"></script>
         </Head>
         <body>
           <Main />
-          <NextScript />
+          <NextScript  />
+          <div>
+            <p className="parag">Get free thumbnail image of any youtube videos in Full HD(1080), HD (720), SD, and also in small size. it's currently supported formats: YouTube (HD, HQ, 1080p, 4K) videos</p>
+            <h1 className="title">What is the use of this YouTube Thumbnail Grabber website?</h1>
+            <p className="parag">People use this YouTube thumbnail downloader website for getting thumbnail from any youtube videos. That can be used in presentation, animation work or many other activities.</p>
+            <h1 className="title">How to use this YouTube thumbnail downloader website?</h1>
+            <p className="parag">It's quite easy just copy any YouTube link whom thumbnail you want. youtube cover download Easily you can download youtube cover.</p>
+            <h1 className="title">Is it legal to download YouTube thumbnails?</h1>
+            <p className="parag">Of course it's 100% legal you can download any YouTube videos thumbnails but as you know thumbnail and video are copyrighted product you should take author permissions 1st in order to reuse that..</p>
+            <h1 className="title">Compatibility of this YT thumbnail grabber website</h1>
+            <p className="parag">This YouTube thumbnail downloader website is going to work well in all devices except iPhone, because iPhone doesn't allow images to be saved in iPhone. But if you using jailbroken iphone then there is no any problem. on the other hand is going to work fine in almost all kinds Android devices and laptop or desktop system..</p>
+          </div>
         </body>
       </Html>
     );
